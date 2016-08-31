5abd62dccadd8d17902f44e3bf570819f069cc14
Software75 <tommaso.dicecco@software75.com>
2016-08-31 16:25:17 +0200

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,12 @@
+<?php
+namespace software75\yii2-ecommerce75;
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'software75\yii2-ecommerce75\controllers';
+	
+    public function init()
+    {
+        parent::init();
+        // custom initialization code goes here        
+    }
+}
